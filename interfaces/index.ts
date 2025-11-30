7819191e9bb4d9f1514d41f1c3dcf236468dc057
Ripcode1 <161825490+Ripcode1@users.noreply.github.com>
2025-12-01 00:00:20 +0200

    Update index.ts

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -1,1 +1,30 @@
+export interface PropertyProps {
+  name: string;
+  rating: number;
+  address: {
+    city: string;
+    country: string;
+    state?: string;
+    street?: string;
+  };
+  image: string;
+  images?: string[];
+  description: string;
+  category: string[];
+  price: number;
+  offers?: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  discount?: string;
+  reviews?: ReviewProps[];
+}
 
+export interface ReviewProps {
+  name: string;
+  avatar: string;
+  rating: number;
+  comment: string;
+  date?: string;
+}
